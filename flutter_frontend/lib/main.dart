30d636bd9b9911f5cd3c954fe2ee114dea67f842
Simão Gato <simao.valedegato@gmail.com>
2024-04-17 19:23:58 +0100

    do refresh functionality for updating mockup products

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -7,8 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   final _productService = ProductService();
 
-  MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     const title = 'Product List';
@@ -20,29 +18,59 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(title),
         ),
-        body: FutureBuilder<List<Product>>(
-          future: _productService.getProducts(),
-          builder: (context, snapshot) {
-            var products = snapshot.data ?? [];
-
-            if(!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            return ListView.builder(
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                var product = products[index];
-                return ListTile(
-                  title: Text(products[index].name),
-                  subtitle: Text('#${product.id} ${product.description}'),
-                  trailing: Text('\$${product.price}')
-                );
-              },
-            );
-          },
-        ),
+        body: ProductList(),
       ),
+    );
+  }
+}
+
+class ProductList extends StatefulWidget {
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  final _productService = ProductService();
+  late Future<List<Product>> _productsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _productsFuture = _productService.getProducts();
+  }
+
+  Future<void> _refreshProducts() async {
+    setState(() {
+      _productsFuture = _productService.getProducts();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Product>>(
+      future: _productsFuture,
+      builder: (context, snapshot) {
+        var products = snapshot.data ?? [];
+
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        return RefreshIndicator(
+          onRefresh: _refreshProducts,
+          child: ListView.builder(
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              var product = products[index];
+              return ListTile(
+                title: Text(products[index].name),
+                subtitle: Text('#${product.id} ${product.description}'),
+                trailing: Text('\$${product.price}'),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
