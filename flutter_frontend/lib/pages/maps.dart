64a9a9c176af8515ea2862957b16226e3e356ce6
Daniel Pereira <72202548+DaniPalma2002@users.noreply.github.com>
2024-04-21 19:30:58 +0100

    Add scrollable draggable sheet to maps

--- flutter_frontend/lib/pages/maps.dart
+++ flutter_frontend/lib/pages/maps.dart
@@ -11,7 +11,8 @@ class MapsPage extends StatefulWidget {
   State<MapsPage> createState() => _MapsPageState();
 }
 
-class _MapsPageState extends State<MapsPage> /*with AutomaticKeepAliveClientMixin*/ {
+class _MapsPageState
+    extends State<MapsPage> /*with AutomaticKeepAliveClientMixin*/ {
   late GoogleMapController mapController;
   StreamSubscription<Position>? _positionListen;
 
@@ -25,7 +26,6 @@ class _MapsPageState extends State<MapsPage> /*with AutomaticKeepAliveClientMixi
   void initState() {
     super.initState();
     _loadPosition().then((value) {
-      print('Current position: $value');
       setState(() {
         _currentPosition = value;
       });
@@ -60,31 +60,80 @@ class _MapsPageState extends State<MapsPage> /*with AutomaticKeepAliveClientMixi
   Widget build(BuildContext context) {
     //super.build(context);
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('PharmacIST'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        //title: const Text('PharmacIST'),
+        backgroundColor: Colors.transparent,
       ),
       body: _currentPosition == null
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : GoogleMap(
-              onMapCreated: _onMapCreated,
-              style: mapTheme,
-              zoomControlsEnabled: false,
-              initialCameraPosition: CameraPosition(
-                target: _currentPosition!,
-                zoom: 17.0,
-              ),
-              markers: {
-                Marker(
-                  markerId: const MarkerId('Your location'),
-                  icon: BitmapDescriptor.defaultMarkerWithHue(120),
-                  position: _currentPosition!,
+          : Stack(children: [
+              GoogleMap(
+                onMapCreated: _onMapCreated,
+                style: mapTheme,
+                zoomControlsEnabled: false,
+                initialCameraPosition: CameraPosition(
+                  target: _currentPosition!,
+                  zoom: 17.0,
                 ),
-              },
-            ),
-      
+                markers: {
+                  Marker(
+                    markerId: const MarkerId('Your location'),
+                    icon: BitmapDescriptor.defaultMarkerWithHue(120),
+                    position: _currentPosition!,
+                  ),
+                },
+              ),
+              DraggableScrollableSheet(
+                initialChildSize: 0.3,
+                minChildSize: 0.2,
+                maxChildSize: 0.8,
+                builder:
+                    (BuildContext context, ScrollController scrollController) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.surface,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 5,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Colors.grey[300],
+                            ),
+                          ),
+                          Flexible(
+                            child: ListView.builder(
+                              physics: const ClampingScrollPhysics(),
+                              padding: const EdgeInsets.all(5),
+                              controller: scrollController,
+                              itemCount: 20,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Card(
+                                  child: ListTile(
+                                    title: Text('Item $index'),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              )
+            ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           mapController.animateCamera(
@@ -143,6 +192,13 @@ class _MapsPageState extends State<MapsPage> /*with AutomaticKeepAliveClientMixi
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _currentPosition = LatLng(position.latitude, position.longitude);
+      _savePosition(_currentPosition!);
+    });
+
     const LocationSettings locationSettings = LocationSettings(
       accuracy: LocationAccuracy.high,
       //distanceFilter: 5,
@@ -152,7 +208,8 @@ class _MapsPageState extends State<MapsPage> /*with AutomaticKeepAliveClientMixi
       print(status);
     });
 
-    _positionListen = Geolocator.getPositionStream(locationSettings: locationSettings)
+    _positionListen =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
             .listen((Position? position) {
       print(position == null
           ? 'Unknown'
