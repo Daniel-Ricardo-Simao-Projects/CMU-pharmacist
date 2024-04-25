1ad8e627f3b23a9f89558fa8d72de381df157184
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-04-25 15:22:43 +0100

    Redesign add pharmacy page

---
+++ flutter_frontend/lib/themes/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xCCD9FDD3);
+const Color primaryBorderColor = Color(0xCC75A386);
+const Color textColor = Color(0xFF193C26);
+const Color accentColor = Color(0xFF193C26);
+const Color backgroundColor = Color(0xffb4feb6);
+//const Color secondaryColor = Color(0xFFE5E5E5);
