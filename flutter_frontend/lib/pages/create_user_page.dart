485f265e9ef7c3ab335a36f9334a3b91c010b503
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-05-13 16:07:19 +0100

    Redesign sign in/up pages

--- flutter_frontend/lib/pages/create_user_page.dart
+++ flutter_frontend/lib/pages/create_user_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/services/user_service.dart';
+import 'package:flutter_frontend/themes/colors.dart';
 
 class CreateUserPage extends StatefulWidget {
   const CreateUserPage({super.key});
@@ -17,30 +18,124 @@ class _CreateUserPageState extends State<CreateUserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryColor,
       appBar: AppBar(
-        title: const Text('Create User'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: text2Color),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          color: text2Color,
+        ),
+        backgroundColor: primaryColor,
+        title: const Text(
+          'Create Username',
+          style: TextStyle(
+            fontFamily: 'JosefinSans',
+            fontVariations: [FontVariation('wght', 700)],
+            color: text2Color,
+            fontSize: 20,
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 45, right: 45),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60),
+                const Icon(Icons.spa, size: 150, color: backgroundColor),
+                const Text(
+                  'PharmacIST',
+                  style: TextStyle(
+                    fontFamily: 'JosefinSans',
+                    fontVariations: [FontVariation('wght', 700)],
+                    color: backgroundColor,
+                    fontSize: 30,
+                  ),
+                ),
+                const SizedBox(height: 60),
+                TextField(
+                  controller: _usernameController,
+                  style: const TextStyle(
+                    fontFamily: 'JosefinSans',
+                    fontVariations: [FontVariation('wght', 400)],
+                    color: text2Color,
+                    fontSize: 15,
+                  ),
+                  cursorColor: backgroundColor,
+                  decoration: const InputDecoration(
+                    hintText: 'Username',
+                    hintStyle: TextStyle(
+                      fontFamily: 'JosefinSans',
+                      fontVariations: [FontVariation('wght', 400)],
+                      color: text2Color,
+                      fontSize: 15,
+                    ),
+                    isDense: true,
+                    contentPadding: EdgeInsets.only(bottom: 10),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: backgroundColor),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: backgroundColor, width: 2),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  style: const TextStyle(
+                    fontFamily: 'JosefinSans',
+                    fontVariations: [FontVariation('wght', 400)],
+                    color: text2Color,
+                    fontSize: 15,
+                  ),
+                  cursorColor: backgroundColor,
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      fontFamily: 'JosefinSans',
+                      fontVariations: [FontVariation('wght', 400)],
+                      color: text2Color,
+                      fontSize: 15,
+                    ),
+                    isDense: true,
+                    contentPadding: EdgeInsets.only(bottom: 10),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: backgroundColor),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: backgroundColor),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: _saveUsername,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(backgroundColor),
+                  ),
+                  child: const Text(
+                    'Create User',
+                    style: TextStyle(
+                      fontFamily: 'JosefinSans',
+                      fontVariations: [FontVariation('wght', 600)],
+                      color: accentColor,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _saveUsername,
-              child: const Text('Create User'),
-            ),
-          ],
+          ),
         ),
       ),
     );
--- flutter_frontend/lib/pages/user_login_page.dart
+++ flutter_frontend/lib/pages/user_login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/main.dart';
 import 'package:flutter_frontend/pages/create_user_page.dart';
+import 'package:flutter_frontend/themes/colors.dart';
 import '../services/user_service.dart';
 
 class LoginPage extends StatefulWidget {
@@ -20,42 +21,135 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: false,
+      backgroundColor: backgroundColor,
       appBar: AppBar(
-        title: const Text('Login'),
+        backgroundColor: backgroundColor,
+        title: const Text(
+          'Login',
+          style: TextStyle(
+            fontFamily: 'JosefinSans',
+            fontVariations: [FontVariation('wght', 700)],
+            color: accentColor,
+            fontSize: 20,
+          ),
+        ),
       ),
       body: _isLoading
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    controller: _usernameController,
-                    decoration: const InputDecoration(labelText: 'Username'),
+          : GestureDetector(
+              onTap: () {
+                FocusScope.of(context).requestFocus(FocusNode());
+              },
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 45, right: 45),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 60),
+                      const Icon(Icons.spa, size: 150, color: primaryColor),
+                      const Text(
+                        'PharmacIST',
+                        style: TextStyle(
+                          fontFamily: 'JosefinSans',
+                          fontVariations: [FontVariation('wght', 700)],
+                          color: primaryColor,
+                          fontSize: 30,
+                        ),
+                      ),
+                      const SizedBox(height: 60),
+                      TextField(
+                        controller: _usernameController,
+                        style: const TextStyle(
+                          fontFamily: 'JosefinSans',
+                          fontVariations: [FontVariation('wght', 400)],
+                          color: text1Color,
+                          fontSize: 15,
+                        ),
+                        cursorColor: primaryColor,
+                        decoration: const InputDecoration(
+                          hintText: 'Username',
+                          hintStyle: TextStyle(
+                            fontFamily: 'JosefinSans',
+                            fontVariations: [FontVariation('wght', 400)],
+                            color: text1Color,
+                            fontSize: 15,
+                          ),
+                          isDense: true,
+                          contentPadding: EdgeInsets.only(bottom: 10),
+                          border: UnderlineInputBorder(),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: primaryColor, width: 2),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        style: const TextStyle(
+                          fontFamily: 'JosefinSans',
+                          fontVariations: [FontVariation('wght', 400)],
+                          color: text1Color,
+                          fontSize: 15,
+                        ),
+                        cursorColor: primaryColor,
+                        decoration: const InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: TextStyle(
+                            fontFamily: 'JosefinSans',
+                            fontVariations: [FontVariation('wght', 400)],
+                            color: text1Color,
+                            fontSize: 15,
+                          ),
+                          isDense: true,
+                          contentPadding: EdgeInsets.only(bottom: 10),
+                          border: UnderlineInputBorder(),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: primaryColor, width: 2),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      ElevatedButton(
+                        onPressed: _login,
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(primaryColor),
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            fontFamily: 'JosefinSans',
+                            fontVariations: [FontVariation('wght', 600)],
+                            color: text2Color,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const CreateUserPage()),
+                        ),
+                        child: const Text(
+                          'Create Username',
+                          style: TextStyle(
+                            fontFamily: 'JosefinSans',
+                            fontVariations: [FontVariation('wght', 400)],
+                            color: accentColor,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 16.0),
-                  TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(labelText: 'Password'),
-                  ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: _login,
-                    child: const Text('Login'),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const CreateUserPage()),
-                    ),
-                    child: const Text('Create Username'),
-                  ),
-                ],
+                ),
               ),
             ),
     );
